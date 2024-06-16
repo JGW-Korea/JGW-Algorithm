55341147871ecad2499a4c4ebc6513e266fcf463
JGW-Korea <94373324+JGW-Korea@users.noreply.github.com>
2024-06-16 15:01:06 +0000

    improve: 백준 스택 실버4 균형잡힌 세상(Test)

--- beakjoon/test.js
+++ beakjoon/test.js
@@ -1,4 +1,27 @@
-const fs = require('fs');
-const input = fs.readFileSync('./beakjoon/index.txt').toString().trim().split(' ').map(Number);
+function solution(str) {
+  const stack = [];
 
-console.log(input);
+  for (const letter of str) {
+    if (!["(", ")", "[", "]"].includes(letter)) continue;
+    else {
+      if (["(", "["].includes(letter)) stack.push(letter);
+      else {
+        if (letter === ")" && stack[stack.length - 1] !== "(") return 'no';
+        else if (letter === "]" && stack[stack.length - 1] !== "[") return 'no';
+        stack.pop();
+      }
+    }
+  }
+  return stack.length ? 'no' : 'yes';
+}
+
+const fs = require("fs");
+const input = fs.readFileSync("index.txt").toString().trim().split("\n");
+
+let result = "";
+
+for(let i = 0; i < input.length - 1; i++) {
+  result += solution(input[i]) + "\n";
+}
+
+console.log(result);
