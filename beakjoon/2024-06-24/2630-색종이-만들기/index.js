62a0ab1c604fdd33f24a20d556065aafde3c5ec4
JGW-Korea <94373324+JGW-Korea@users.noreply.github.com>
2024-07-21 11:25:51 +0000

    improve(solve): 백준 재귀 Silver 색종이 만들기

--- beakjoon/2024-06-24/2630-색종이-만들기/index.js
+++ beakjoon/2024-06-24/2630-색종이-만들기/index.js
@@ -1,50 +1,40 @@
-function solution(n, paper) {
+function solution(N, papers) {
+  // 흰색, 파란색 색종이 개수
+  let whitePaperCount = 0; 
+  let bluePaperCount = 0;
 
-  const size = [128, 64, 32, 16, 8, 4, 2, 1].filter(element => element <= n);
-  let blueColorCount = 0;
-  let whiteColorCount = 0;
+  function divide(row, col, length) {
+    let sum = 0;
 
-  size.forEach((element) => {
-
-    for(let i = 0; i <= n - element; i += element) {
-      for(let j = 0; j <= n - element; j += element) {
-
-        let color = paper[i][j];
-        let flag = false;
-
-        outer:
-        for(let x = i; x < i + element; x++) { 
-          for(let y = j; y < j + element; y++) {
-            if(paper[x][y] === 2 || paper[x][y] !== color) {
-              flag = true;
-              break outer;
-            }
-          }
-        }
-
-        if(!flag) {
-          if(color === 0) whiteColorCount += 1;
-          else if(color === 1) blueColorCount += 1;
-
-          for(let x = i; x < i + element; x++) {
-            for(let y = j; y < j + element; y++) {
-              paper[x][y] = 2;
-            }
-          }
-          
-        }
+    // 현재 색종이의 영역의 상태를 구한다.
+    for(let i = row; i < row + length; i++) {
+      for(let j = col; j < col + length; j++) {
+        sum += papers[i][j];
       }
     }
-    
-  });
 
-  return whiteColorCount + "\n" + blueColorCount;
+    if(sum === length * length) bluePaperCount += 1; // 현재 색종이의 영역 상태가 파란색 종이만 있을 경우
+    else if(sum === 0) whitePaperCount += 1; // 현재 색종이의 영역 상태가 흰색 종이만 있을 경우
+    else {
+      length = Math.floor(length / 2); // 현재 종이의 2/N 값을 계산한다.
+
+      divide(row, col, length); // 1사분면
+      divide(row, col + length, length); // 2사분면
+      divide(row + length, col, length); // 3사분면
+      divide(row + length, col + length, length); // 4사분면
+      
+    }
+  }
+
+  divide(0, 0, N);
+  
+  return whitePaperCount + "\n" + bluePaperCount;
 }
 
 const fs = require('fs');
 const input = fs.readFileSync('index.txt').toString().trim().split('\n');
 
-const n = Number(input[0]);
-const paper = input.slice(1).map(element => element.split(' ').map(Number));
+const N = Number(input[0]);
+const papers = input.slice(1).map(element => element.split(' ').map(Number));
 
-console.log(solution(n, paper));
+console.log(solution(N, papers));
