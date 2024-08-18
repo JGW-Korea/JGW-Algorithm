193a93faf73ee152a674c4f521ea56dc0949b8ca
JGW-Korea <94373324+JGW-Korea@users.noreply.github.com>
2024-08-18 11:30:58 +0000

    improve: 백준 BFS/DFS Gold 벽 부수고 이동하기 2

--- beakjoon/2024-08-15/17219-비밀번호-찾기/index.js
+++ beakjoon/2024-08-15/17219-비밀번호-찾기/index.js
@@ -5,13 +5,11 @@ const [N, M] = input[0].split(' ').map(Number);
 const memos = input.slice(1, N + 1).map(element => element.split(' '));
 const targets = input.slice(N + 1);
 
-const notepad = {};
+const notepad = {}; // 객체를 이용해서 사이트의 주소와 비밀번호를 담는다.
 let result = "";
 
-memos.forEach(([memo, pwd]) => {
-  notepad[memo] = pwd;
-});
+memos.forEach(([site, pwd]) => notepad[site] = pwd); // 사이트의 주소를 Key, 비밀번호를 Value로 담는다.
 
-targets.forEach(target => result += notepad[target] + "\n");
-
+// 저장된 사이트 주소의 원소를 통해서 객체의 Key에 접근을 해서 Value 값을 넣어준다.
+targets.forEach(element => result += notepad[element] + "\n");
 console.log(result);
