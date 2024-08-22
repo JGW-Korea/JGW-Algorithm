ce61aa4d67243ac7b77875641c6aa70e77ea7919
JGW-Korea <94373324+JGW-Korea@users.noreply.github.com>
2024-08-22 09:17:10 +0000

    fail: 백준 수학 Silver 팩토리얼 0의 개수

---
+++ beakjoon/2024-08-22/1676-팩토리얼-0의-개수/index.js
@@ -0,0 +1,8 @@
+function solution(N) {
+  return Math.floor(N / 5) + Math.floor(N / 25) + Math.floor(N / 125);
+}
+
+const fs = require('fs');
+const input = Number(fs.readFileSync('index.txt').toString().trim());
+
+console.log(solution(input));
